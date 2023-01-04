b3c5c55a160f05eddd410dc1eb840be7082cb74b
SkynixFr <romain.ludovic.day@gmail.com>
2023-01-04 17:21:12 +0100

    feat(index): configuration du serveur express et route par défaut

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import * as dotenv from 'dotenv';
+import express, { Router, Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+//Récupération des données dans le fichier .env
+dotenv.config();
+if (!process.env.PORT) {
+	process.exit(1);
+}
+const PORT: number = parseInt(process.env.PORT);
+
+//Lancement du serveur Express
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+//Initial route
+app.get('/', (req: Request, res: Response) => {
+	res.json({
+		message: 'Pokepi !'
+	});
+});
+
+try {
+	app.listen(PORT, () => {
+		console.log(`Server is running on http://localhost:${PORT}`);
+	});
+} catch (error) {
+	console.error(error);
+}
