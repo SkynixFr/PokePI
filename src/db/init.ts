1e878c0cd1e74110c8d6be5ad9cfb8897f46aef1
Ricky Khao <khaoricky@gmail.com>
2023-01-05 14:27:33 +0100

    feat (pokemon): ajout index et route pokemon

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,7 +1,9 @@
 import Client from '../models/client.interface';
+import Pokemon from '../models/pokemon.interface';
 
 const dbInit = () => {
 	Client.sync();
+	Pokemon.sync();
 };
 
 export default dbInit;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import clientsRouter from './clients.routes';
+import PokemonRouter from './pokemon.routes';
 
 const router = Router();
 
 router.use('/clients', clientsRouter);
+router.use('/pokemon', PokemonRouter);
 
 export default router;
